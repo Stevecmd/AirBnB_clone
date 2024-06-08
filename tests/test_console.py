b6669b35dbd74b66063c43d90ef4a7aeae3359d6
Stevecmd <murimi101@gmail.com>
2024-06-08 15:08:31 +0300

    Added extra line

--- tests/test_console.py
+++ tests/test_console.py
@@ -12,6 +12,7 @@ from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
 
+
 class TestHBNBCommand(unittest.TestCase):
     def setUp(self):
         self.cli = HBNBCommand()
